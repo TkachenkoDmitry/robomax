e0c0f883258586e3bc70541b7a20dad0f73dad62
Tkachenko Dmitrii <tkachenko.dmitrii.vlad@gmail.com>
2019-09-13 13:16:52 +0200

    fixed form display

--- components/contactForm.js
+++ components/contactForm.js
@@ -3,52 +3,51 @@ import Input from './UI/Input';
 
 const contactForm = () => {
   return (
-    <section className='full-bleed contact'>
-      <div className='contact-form'>
-        <picture>
-          <source type='image/png' />
-          <img src='../static/images/robot.png' alt='icon' />
-        </picture>
-        <div>
-          {/* <a href='/' className='button'>
+    <section className='contact-form'>
+      <picture>
+        <source type='image/png' />
+        <img src='../static/images/robot.png' alt='icon' />
+      </picture>
+      <div>
+        {/* <a href='/' className='button'>
             button!
           </a> */}
-          <form action=''>
-            <h2>Contact us</h2>
-            <p>
-              Оставьте заявку на аренду, выбрав интересующую Вас дату!
-              (календарь и форма обратной связи: телефон, имя) Наша компания, в
-              свою очередь, окажет техническое сопровождение и консультации по
-              мере необходимости
-            </p>
-            <br />
-            <br />
-            <Input
-              input={{}}
-              label='Name'
-              type='text'
-              meta={{ touched: false }}
-            />
-            <Input
-              input={{}}
-              label='Email'
-              type='email'
-              meta={{ touched: false }}
-            />
-            <Input
-              input={{}}
-              label='Phone'
-              type='phone'
-              meta={{ touched: false }}
-            />
-            <Input
-              input={{}}
-              label='Message'
-              type='text'
-              meta={{ touched: false }}
-            />
-          </form>
-        </div>
+        <form action=''>
+          <h2>Contact us</h2>
+          <p>
+            Оставьте заявку на аренду, выбрав интересующую Вас дату! (календарь
+            и форма обратной связи: телефон, имя) Наша компания, в свою очередь,
+            окажет техническое сопровождение и консультации по мере
+            необходимости
+          </p>
+          <br />
+          <br />
+          <Input
+            input={{}}
+            label='Name'
+            type='text'
+            meta={{ touched: false }}
+          />
+          <Input
+            input={{}}
+            label='Email'
+            type='email'
+            meta={{ touched: false }}
+          />
+          <Input
+            input={{}}
+            label='Phone'
+            type='phone'
+            meta={{ touched: false }}
+          />
+          <Input
+            input={{}}
+            label='Message'
+            type='text'
+            meta={{ touched: false }}
+          />
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     </section>
   );
